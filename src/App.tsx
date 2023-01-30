99a0040d5177fd8d96fd8127205b3afa6dc13d4a
fabiojunik <fabiojunik@gmail.com>
2023-01-30 07:58:09 +0100

    feat: create app style

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,13 @@
+import { ThemeProvider } from "styled-components"
+import { GlobalStyle } from "./styles/global"
+import { defaultTheme } from "./styles/themes/default"
 
 
 export function App() {
   return (
-    <h1>FJ Timer</h1>
+    <ThemeProvider theme={defaultTheme}>
+      <GlobalStyle />
+      <h1>FJ Timer</h1>
+    </ThemeProvider>
   )
 }
