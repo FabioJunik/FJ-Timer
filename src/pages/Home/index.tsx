987bd838e7d552fe9bf079db6ac5f1eb6c617924
fabiojunik <fabiojunik@gmail.com>
2023-01-31 08:16:28 +0100

    feat: improve input

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -12,13 +12,24 @@ export function Home() {
                         type="text"
                         id="task"
                         placeholder="Dê um nome para o teu projeto"
+                        list="task-suggestions"
                     />
+
+                    <datalist id="task-suggestions">
+                        <option value="Projeto 1"></option>
+                        <option value="Projeto 2"></option>
+                        <option value="Projeto 3"></option>
+                        <option value="Projeto 4"></option>
+                    </datalist>
 
                     <label htmlFor="minutesAmount">durante</label>
                     <MinutesAmountInput
                         type="number"
                         id="minutesAmount"
                         placeholder="00"
+                        step={5}
+                        min={5}
+                        max={60}
                     />
 
                     <span>minutes.</span>
